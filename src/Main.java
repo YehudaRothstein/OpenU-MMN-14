6c072a2d05d6517c023524bb96c8326cd9e25079
Team <yehudarothstein@gmail.com>
2025-01-26 19:08:50 +0200

    added recursive algorithm gor data transfer

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1,2},
+                {3,4}
+        };
+        MatrixList matrixList = new MatrixList(arr);
+        System.out.println(matrixList.toString());
+
+    }
+}
